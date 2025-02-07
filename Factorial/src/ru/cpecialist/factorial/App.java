ba6d29fb08b440f3882911c6d10070f0ab60f695
Николай Шевцов <mollpromking@hmail.com>
2025-02-07 22:04:38 +0300

    Лабораторная работа № 5.1

--- Factorial/src/ru/cpecialist/factorial/App.java
+++ Factorial/src/ru/cpecialist/factorial/App.java
@@ -3,21 +3,24 @@ package ru.cpecialist.factorial;
 import java.util.Scanner;
 
 public class App {
-
-	public static void main(String[] args) {
-		
-		System.out.println("Введите число для вычисления факториала: ");
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		int fact = 1;
-		
-		// меняет - на +
+	
+	public static int fact = 1;
+	public static int factorial(int n) {
 		if(n < 0)
 			n = -n;
 		for(int i = 1; i <= n; i++) {
 			fact *= i;
 		}
-		System.out.print(fact);
+		return fact;
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("Введите число для вычисления факториала: ");
+		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+			
+		System.out.print(factorial(x));
 
 	}
 
